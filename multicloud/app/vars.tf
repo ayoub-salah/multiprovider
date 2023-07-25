77dbf77077e6b14bfbd00ec20281659d2a0d4e32
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-25 23:32:15 +0200

    Update vars.tf

--- multicloud/app/vars.tf
+++ multicloud/app/vars.tf
@@ -38,7 +38,16 @@ variable "instances_aws" {
 
 //***********************************************************VARS FOR AZURE 
 variable "rg" {
-  description = "rg where subnets are deployed "
-  type        = string
-  default     = "1-93985c2a-playground-sandbox"
+  type    = string
+  default = "1-2d254977-playground-sandbox"
+}
+
+variable "subscription_id" {
+  type    = string
+  default = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
+}
+
+variable "availability_zone" {
+  type    = string
+  default = "East us"
 }
