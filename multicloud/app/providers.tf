b7e29ba963db13c32ceb9a22b8c8833ad90d0fae
AyoubCy <69630622+ayoub-salah@users.noreply.github.com>
2023-07-27 01:54:02 +0200

    add aws provider block

--- multicloud/app/providers.tf
+++ multicloud/app/providers.tf
@@ -3,3 +3,8 @@ provider "google" {
   project     = jsondecode(file("creds.json")).project_id
   region      = "europe-west1"
 }
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA2FTTSXCQAHLVADNP"
+  secret_key = "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"
+}
