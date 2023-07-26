534fafad9da36614c9af9ffbbcea1c242d059f0f
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-26 17:10:15 +0200

    Update variables.tf

--- multicloud/modules/vpc/azure/variables.tf
+++ multicloud/modules/vpc/azure/variables.tf
@@ -1,18 +1,2 @@
-# variable "rg" {
-#   type    = string
-# }
 variable "vpcs" {
-    type            = map(object({
-    name            = string
-    address_space   = list(string)
-    location        = string
-    resource_group  = string
-   }))
 }
-# variable "nics" {
-#   type = map(object({
-#     subnet_id                     = string
-#     private_ip_address_allocation = string
-#   }))
-#   default = {}
-# }
