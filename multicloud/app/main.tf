d667f823a02d3d8b24cc09e52e7ffd83e2bedb26
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-26 17:02:30 +0200

    Update main.tf

--- multicloud/app/main.tf
+++ multicloud/app/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
 provider "azurerm" {
   # Configuration options
   subscription_id            = var.subscription_id
@@ -19,76 +10,102 @@ module "vpc" {
   # rg     = var.rg
   vpcs = {
     "vpc1" = {
-      name          = "vpc1"
-      address_space = ["10.0.0.0/16"]
-      location      = var.location
-      resource_group = var.rg
+      name              = "vpc1"
+      address_space     = ["10.0.0.0/16"]
+      availability_zone = var.availability_zone
+      resource_group    = var.rg
+    },
+    "vpc2" = {
+      name              = "vpc2"
+      address_space     = ["10.1.0.0/16"]
+      availability_zone = var.availability_zone
+      resource_group    = var.rg
+
     }
-    
+
   }
 
 }
 
 module "subnet" {
-  source = "../modules/subnets/azure"
+  source = "../modules/subnet/azure"
   # rg     = var.rg
   subnets = {
     subnet1 = {
-      name             = "subnet1"
-      address_prefixes = ["10.0.2.0/24"]
-      resource_group   = var.rg
-
+      name           = "subnet1"
+      cidr_block     = ["10.0.2.0/24"]
+      resource_group = var.rg
+      vpc_name       = module.vpc.vpc_name["vpc1"]
+    },
+    subnet2 = {
+      name           = "subnet2"
+      cidr_block     = ["10.1.3.0/24"]
+      resource_group = var.rg
+      vpc_name       = module.vpc.vpc_name["vpc2"]
     }
-   
-  }
-  vpc_name = module.vpc.vpc_name["vpc1"]
-  #  vpc_name = module.vpc.vpc_name["vpc2"]
-}
 
+  }
+}
 
 module "vms" {
   source = "../modules/vm/azure"
 
   vms = {
     "vm1" = {
-      resource_group                 = var.rg
-      location                       = "East US"
-      size                           = "Standard_F2"
-      admin_username                 = "adminuser"
+      name                            = "myVm1"
+      resource_group                  = var.rg
+      availability_zone               = "East US"
+      size                            = "Standard_F2"
+      admin_username                  = "adminuser"
       disable_password_authentication = false
-      admin_password                 = "Abc_123###"
-      public_ip                      = true
+      admin_password                  = "Abc_123###"
+      public_ip                       = true
       # subnet_id                      = module.subnet.subnet_ids["subnet1"].id
-      subnet_id                      = module.subnet.subnet_ids["subnet1"]
-      private_ip_address_allocation  = "Dynamic"
+      subnet_id                              = module.subnet.subnet_ids["subnet1"]
+      private_ip_address_alavailability_zone = "Dynamic"
+      myscript                               = file("./myScript.sh")
+      rule = {
+        priority   = 120,
+        open_ports = tolist(["80"])
+      }
     },
     "vm2" = {
-      resource_group                 = var.rg
-      location                       = "East US"
-      size                           = "Standard_F2"
-      admin_username                 = "adminuser"
-      disable_password_authentication = false
-      admin_password                 = "Abc_123###"
-      public_ip                      = true
-      subnet_id                      = module.subnet.subnet_ids["subnet1"]
-      private_ip_address_allocation  = "Dynamic"
+      name                                   = "myVm2"
+      resource_group                         = var.rg
+      availability_zone                      = "East US"
+      size                                   = "Standard_F2"
+      admin_username                         = "adminuser"
+      disable_password_authentication        = false
+      admin_password                         = "Abc_123###"
+      public_ip                              = true
+      subnet_id                              = module.subnet.subnet_ids["subnet1"]
+      private_ip_address_alavailability_zone = "Dynamic"
+      myscript                               = file("./myScript.sh")
+      rule = {
+        priority   = 130,
+        open_ports = tolist(["*"])
+      }
     },
     "vm3" = {
-      resource_group                 = var.rg
-      location                       = "East US"
-      size                           = "Standard_F2"
-      admin_username                 = "adminuser"
-      disable_password_authentication = false
-      admin_password                 = "Abc_123###"
-      public_ip                      = false
-      subnet_id                      = module.subnet.subnet_ids["subnet1"]
-      private_ip_address_allocation  = "Dynamic"
-    },
+      name                                   = "myVm3"
+      resource_group                         = var.rg
+      availability_zone                      = "East US"
+      size                                   = "Standard_F2"
+      admin_username                         = "adminuser"
+      disable_password_authentication        = false
+      admin_password                         = "Abc_123###"
+      public_ip                              = false
+      subnet_id                              = module.subnet.subnet_ids["subnet2"]
+      private_ip_address_alavailability_zone = "Dynamic"
+      myscript                               = file("./myScript.sh")
+      rule = {
+        priority   = 140,
+        open_ports = tolist([80, 252])
+      }
+
+    }
   }
 }
-
-
-  
 
 
 
