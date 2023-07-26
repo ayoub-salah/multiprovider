d21d4cdb589b147f35938c5edc18c34cb5b5e011
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-26 17:09:11 +0200

    Update main.tf

--- multicloud/modules/vpc/azure/main.tf
+++ multicloud/modules/vpc/azure/main.tf
@@ -1,21 +1,7 @@
 resource "azurerm_virtual_network" "vNet" {
-
   for_each            = var.vpcs
   name                = each.key
   resource_group_name = each.value.resource_group
-  location            = each.value.location
-  # address_space     = ["10.0.0.0/16"]
+  location           = each.value.availability_zone
   address_space       = each.value.address_space
 }
-# resource "azurerm_network_interface" "nic" {
-#   for_each            = var.nics
-#   name                = "myNic-${each.key}"
-#   location            = var.vpcs[each.key].location
-#   resource_group_name = var.rg
-
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = each.value.subnet_id
-#     private_ip_address_allocation = each.value.private_ip_address_allocation
-#   }
-#   }
