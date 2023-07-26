369a75390bc780f1e2d9026c12f17da768f613d2
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-26 17:07:02 +0200

    Update main.tf

--- multicloud/modules/subnets/azure/main.tf
+++ multicloud/modules/subnets/azure/main.tf
@@ -1,9 +1,7 @@
 resource "azurerm_subnet" "subnet" {
   for_each               = var.subnets
   name                   = each.key
-  address_prefixes       = each.value.address_prefixes
-  # resource_group_name    = var.rg
+  address_prefixes       = each.value.cidr_block
   resource_group_name    = each.value.resource_group
-  virtual_network_name   = var.vpc_name
+  virtual_network_name   = each.value.vpc_name
 }
-# address_prefixes = ["10.0.1.0/24"]
