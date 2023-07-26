be3757cb7bfc3a177d4c833d0ee1b4d858343a73
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-26 17:07:49 +0200

    Update variables.tf

--- multicloud/modules/subnets/azure/variables.tf
+++ multicloud/modules/subnets/azure/variables.tf
@@ -1,12 +1,1 @@
-variable "subnets" {
-  type    = map(object({
-      name             = string
-      address_prefixes = list(string)
-      resource_group   = string
-  }))
-  default = {}
-}
-variable "vpc_name" {
-  type        = string
-}
-  
+variable "subnets" {}
