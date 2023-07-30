8aad579cc2bb329ce7bee905ff1943d913b3c765
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-30 17:17:21 +0200

    Update Azure.tf

--- multicloud/app/Azure.tf
+++ multicloud/app/Azure.tf
@@ -1,17 +1,3 @@
-provider "azurerm" {
-  # Configuration options
-  subscription_id            = var.subscription_id
-  skip_provider_registration = true
-  features {}
-}
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      # version = "=3.0.0"
-    }
-  }
-}
 module "vpc" {
   source = "../modules/vpc/azure"
   # rg     = var.rg
