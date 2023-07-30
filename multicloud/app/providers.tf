613c0c62d4f94d5fe7b02c39f41bfd97617e9345
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-30 17:17:55 +0200

    Update providers.tf

--- multicloud/app/providers.tf
+++ multicloud/app/providers.tf
@@ -8,3 +8,9 @@ provider "aws" {
   access_key = "AKIA2FTTSXCQAHLVADNP"
   secret_key = "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"
 }
+provider "azurerm" {
+  # Configuration options
+  subscription_id            = var.subscription_id
+  skip_provider_registration = true
+  features {}
+}
