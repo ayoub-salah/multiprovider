8244378b1eae4d60889aecf2195ed2209bbca8cb
SimoTaf <79705929+SimoTaf@users.noreply.github.com>
2023-07-30 17:32:54 +0200

    Update main.tf

---
+++ multicloud/modules/subnets/azure/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_subnet" "subnet" {
+  for_each             = var.subnets
+  name                 = each.key
+  address_prefixes     = each.value.cidr_block
+  resource_group_name  = each.value.resource_group
+  virtual_network_name = each.value.vpc_name
+}
